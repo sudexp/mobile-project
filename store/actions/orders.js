c484c892255ba004aed85749e3987dac1858c1f4
sudexp <sudexp@icloud.com>
2019-12-11 20:04:57 +0200

    SubmitOrder action fixed

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -12,16 +12,16 @@ export const submitOrder = (token, orderId, userData) => {
   return async (dispatch, getState) => {
     // Save order - address, and isCompleted=true
     await saveOrder(token, orderId, userData);
-    return {
+    return dispatch({
       type: SUBMIT_ORDER,
       userData: userData,
-    };
+    });
   };
 };
 
 async function saveOrder(token, orderId, userData) {
   console.log(
-    `[saveOrder] token=${token}, orderId=${orderId}, userData: `,
+    `[saveOrder] saving... token=${token}, orderId=${orderId}, userData: `,
     userData,
   );
   const response = await fetch(
@@ -43,7 +43,10 @@ async function saveOrder(token, orderId, userData) {
 
   if (!response.ok) {
     const errorResponceData = await response.json();
-    console.log('[saveOrder]: ', errorResponceData);
+    console.log(
+      '[saveOrder]: Was not able to complete the order:',
+      errorResponceData,
+    );
     throw new Error('Was not able to complete the order!');
   }
 
